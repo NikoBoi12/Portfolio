95d016c335a0014b0368b8b935cc1ec70260a1e6
NikoBoi12 <150070653+NikoBoi12@users.noreply.github.com>
2025-01-17 14:14:24 -0600

    Update PickUp.c

--- ProjectSouls/Unity(W.I.P)/PickUp.c
+++ ProjectSouls/Unity(W.I.P)/PickUp.c
@@ -1,1 +1,34 @@
 
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Rendering;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    GameObject head;
+
+    void Start()
+    {
+        head = transform.GetChild(0).GameObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit itemHit;
+            if (Physics.Raycast(head.transform.position, ray.direction, out itemHit, 5)) {
+                ItemProps itemProps = itemHit.transform.gameObject.GetComponent<ItemProps>();
+                if (itemProps) {
+                    Debug.Log(itemProps.item);
+                    itemProps.item.SetActive(true);
+                    itemHit.transform.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
